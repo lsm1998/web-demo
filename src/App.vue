99192c7b6d48cc0f3260e85920abd523c76b2187
p8s8liushim <p8s8liushim@yujing.live>
2023-01-12 11:01:07 +0800

    nut ui

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,57 @@
 <template>
   <div>
-    你好，我是pinia -- {{useStore.age }}
+    你好，我是pinia -- {{ useStore.age }}
     <p> doubleAge = {{ useStore.doubleAge }}</p>
     <p> getNameAndAge = {{ useStore.getNameAndAge }}</p>
     <p> getAddAge = {{ useStore.getAddAge(10) }}</p>
     <button @click="add">+1</button>
+    <nut-button type="info" @click="changeName">修改名字</nut-button>
+  </div>
 
-    <button @click="changeName">修改名字</button>
+  <div>
+    <nut-cell title="显示中文" :desc="desc" @click="show = true"></nut-cell>
+    <nut-datepicker
+        v-model="currentDate"
+        v-model:visible="show"
+        :min-date="minDate"
+        :max-date="maxDate"
+        :is-show-chinese="true"
+        :three-dimensional="false"
+        @confirm="confirm"
+    ></nut-datepicker>
   </div>
 </template>
 
 <script setup lang="ts">
 
 import {useUserStore} from "./store/user"
+import {ref} from "vue";
+import {PickerOption} from "@nutui/nutui/dist/types/__VUE/picker/types";
 
 const useStore = useUserStore()
 
-const add = ()=>{
-  useStore.age ++;
+const add = () => {
+  useStore.age++;
 }
 
-const changeName = ()=>{
+const changeName = () => {
   useStore.setName('new name')
+}
+
+const show = ref(false);
+const desc = ref('2022年05月10日');
+
+const minDate = new Date(2020, 0, 1);
+const maxDate = new Date(2025, 10, 1);
+const currentDate = new Date(2022, 4, 10, 10, 10);
+
+const confirm = ({selectedValue, selectedOptions}: {
+  selectedValue: (string | number)[];
+  selectedOptions: PickerOption[];
+}) => {
+  desc.value = selectedOptions.map((option: PickerOption) => option.text).join('');
 }
 </script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,9 @@ import App from './App.vue'
 
 import pinia from './store'
 
-createApp(App).use(pinia).mount('#app')
+// 注意：这种方式将会导入所有组件
+import NutUI from "@nutui/nutui";
+// 采用按需加载时  此全局css样式，需要删除
+import "@nutui/nutui/dist/style.css";
+
+createApp(App).use(NutUI).use(pinia).mount('#app')
